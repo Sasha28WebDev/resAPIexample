df3ba3aeec835e86be00265ce05652892275c8ba
Sasha28WebDev <Sasha28WebDev@gmail.com>
2022-06-08 18:39:33 +0300

    added README

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,7 +3,7 @@ const router = new Router()
 const auth = require('../controllers/auth.controller')
 
 router.post('/login',auth.loginToService);
-//router.post('/token', auth.tokenGenerate);
+router.post('/token', auth.tokenGenerate);
 router.post('/logout', auth.logoutService);
 
 module.exports = router
